8c81ec4863765198db360a36214574fbed4a69aa
DanilKulak <1998kylak@gmail.com>
2023-11-01 00:40:28 +0200

    done-task01-and-task02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+document.querySelector('.gallery').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains('gallery__image')) {
+        const largeImageSource = event.target.getAttribute('data-source');
+        const lightbox = basicLightbox.create(`
+        <img src="${largeImageSource}" wigth="800" height="600">
+        `);
+
+        lightbox.show();
+    }
+});
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        basicLightbox.close();
+    }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+document.querySelector('.gallery').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains('gallery__image')) {
+        const largeImageSource = event.target.getAttribute('data-source');
+        const lightbox = basicLightbox.create(`
+        <img src="${largeImageSource}" wigth="800" height="600">
+        `);
+
+        lightbox.show();
+    }
+});
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        basicLightbox.close();
+    }
+});
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
